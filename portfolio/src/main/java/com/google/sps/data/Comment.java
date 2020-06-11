ff04df1890414b06dbac41553033623f41a684c8
QuinnDaCollins <QuinnDaCollins@gmail.com>
2020-06-11 17:51:19 -0500

    Remove Date package from Comment class

--- portfolio/src/main/java/com/google/sps/data/Comment.java
+++ portfolio/src/main/java/com/google/sps/data/Comment.java
@@ -12,8 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 package com.google.sps.data;
-import java.util.Date;
-
 public final class Comment{
     private final Object user;
     private final Object content;
