b512dc938356c6bee992de5158e9c14b63300455
quinndacollins <quinndacollins@gmail.com>
2020-06-12 19:49:30 +0000

    Add some centering and styling to comments

--- portfolio/src/main/java/com/google/sps/data/Comment.java
+++ portfolio/src/main/java/com/google/sps/data/Comment.java
@@ -12,6 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 package com.google.sps.data;
+import java.util.Date;
 public final class Comment{
     private final Object user;
     private final Object content;
